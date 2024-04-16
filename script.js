f77709d537c515bc0bf2c1adb5068958361528b9
MateusL21 <mateusleonel27@gmail.com>
2024-04-16 14:36:03 -0300

    closes #3 - tratar o erro se o usuario nao existir

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
-function search(){
+function search() {
 
     var username = document.getElementById("inputUserName").value;
 
     var url = `https://api.github.com/users/${username}`
 
     $.getJSON(url, (user) => {
+        showUserData(user);
+        clearError();
+    }).fail(() => {
+        showUserData({})
+        showError("Não encontrado!");
+    });
+}
 
-        document.getElementById("name").innerHTML     = user.name;
-        document.getElementById("html_url").innerHTML = user.html_url;
-        document.getElementById("company").innerHTML  = user.company;
+function showError(msg) {
+    document.getElementById("error").innerHTML = `<div class='alert alert-danger mt-1' role='alert'>${msg}</div>`;
+}
 
-        document.getElementById("avatar_url").innerHTML  = `<img  src=${user.avatar_url} width="220" height="220"
-        class="shadow rounded"></img>`
+function clearError() {
+    document.getElementById("error").innerHTML = "";
+}
 
+function showUserData(user) {
 
-});
+    document.getElementById("name").innerHTML = user.name || "";
+    document.getElementById("html_url").innerHTML = user.html_url || "";
+    document.getElementById("company").innerHTML = user.company || "";
+
+    document.getElementById("avatar_url").innerHTML = user.avatar_url ?
+        `
+                                                        <img  src=${user.avatar_url} width="220" height="220" class="shadow rounded"></img>
+                                                        `:
+        "";
 }
